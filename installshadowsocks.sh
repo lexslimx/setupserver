f4e4ffdddffb130a3fcdbcc434c794f463eec101
alex <alex.km@live.com>
2020-08-28 15:50:47 +0400

    first

---
+++ installshadowsocks.sh
@@ -0,0 +1,32 @@
+#Create a new system user for Shadowsocks:
+adduser --system --no-create-home --group shadowsocks
+
+#Create a new directory for the configuration file:
+mkdir -m 755 /etc/shadowsocks
+
+#copy local conf file
+cp shadowsocks.json /etc/shadowsocks/shadowsocks.json
+
+#Optimize ShadowsocksPermalink
+cp local.conf /etc/sysctl.d/local.conf
+
+#Apply optimizations:
+sysctl --system
+
+#copy systemd file
+cp shadowsocks.service /etc/systemd/system/shadowsocks.service
+
+#Enable and start shadowsocks.service:
+systemctl daemon-reload
+systemctl enable shadowsocks
+systemctl start shadowsocks
+
+#iptables
+iptables -4 -A INPUT -p tcp --dport 443 -m comment --comment "Shadowsocks server listen port" -j ACCEPT
+
+#ufw
+ufw allow proto tcp to 0.0.0.0/0 port 443 comment "Shadowsocks server listen port"
+
+#firewall
+firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" port protocol="tcp" port="8388" accept'
+firewall-cmd --reload
